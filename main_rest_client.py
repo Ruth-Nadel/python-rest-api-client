cfc02e4dbf3adffa8cb018ac2076cfa6bda9707b
Ruth-Nadel <75452128+Ruth-Nadel@users.noreply.github.com>
2023-11-13 11:56:27 +0200

    Update and rename rest_client.py to main_rest_client.py
    insert code to function

---
+++ main_rest_client.py
@@ -0,0 +1,33 @@
+from rest_api_client import RESTAPIClient
+import json
+
+
+def main(url="https://resttest10.herokuapp.com"):
+    # Create an instance of the RESTAPIClient
+    api_client = RESTAPIClient(url)
+
+    # Step 1: Receive JSON by making two API calls
+    json_response_1 = api_client.get_json(1)
+    print("JSON Response for Serial=1:")
+    print(json.dumps(json_response_1, indent=2))
+    print("\n")
+    json_response_2 = api_client.get_json(2)
+    print("JSON Response for Serial=2:")
+    print(json.dumps(json_response_2, indent=2))
+    print("\n")
+
+    # Step 2: Process JSON
+    processed_data = api_client.process_json(json_response_1, json_response_2)
+
+    # Step 3: Send processed JSON
+    response = api_client.send_processed_json(processed_data)
+
+    # Check if the processing was successful
+    assert response.status_code == 200, f"Error in processing. Status Code: {response.status_code}. Response: {response.text}"
+    assert response.json() == "correct", f"Processing was not successful. Response: {response.json()}"
+
+    print("Processing successful. Message: correct")
+
+
+if __name__ == "__main__":
+    main()
