2ae33395be2c85649a5c3de887de5191e780a6d2
Ruth-Nadel <75452128+Ruth-Nadel@users.noreply.github.com>
2023-11-13 11:08:04 +0200

    Create rest_api_client.py
    first creation rest file

---
+++ rest_api_client.py
@@ -0,0 +1,76 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+class RESTAPIClient:
+    def __init__(self, base_url):
+        self._base_url = base_url
+        self._session = self._create_session()
+
+    @property
+    def base_url(self):
+        return self._base_url
+
+    @property
+    def session(self):
+        return self._session
+
+    def _create_session(self):
+        session = requests.Session()
+        adapter = requests.adapters.HTTPAdapter(
+            max_retries=Retry(total=3, backoff_factor=1, status_forcelist=[500, 502, 503, 504]))
+        session.mount("http://", adapter)
+        session.mount("https://", adapter)
+        session.timeout = (3, 30)  # Set a longer timeout (connect timeout, read timeout)
+        return session
+
+    def get_json(self, serial):
+        try:
+            response = self.session.get(f"{self.base_url}/api/responses?serial={serial}")
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RetryError as retry_err:
+            if "503 error responses" in str(retry_err):
+                print(f"Too many 503 errors occurred while fetching JSON for serial {serial}")
+                # Handle the situation with too many 503 errors as needed
+            else:
+                print(f"Retry error occurred while fetching JSON for serial {serial}: {str(retry_err)}")
+                raise  # Re-raise the exception for non-503 errors
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while fetching JSON for serial {serial}: {str(e)}")
+            raise  # Re-raise the exception to propagate it to the calling code
+
+    def process_json(self, json_response_1, json_response_2):
+        try:
+            processed_data = {
+                "serial": 3,
+                "message": {
+                    "subset": {
+                        "general": {
+                            "information": {
+                                "date": "1-2-2021",
+                                "version": "3.00"
+                            },
+                            "quantities": {
+                                "first": max(json_response_1["message"]["subset"]["general"]["quantities"]["first"],
+                                            json_response_2["message"]["subset"]["general"]["quantities"]["first"]),
+                                "second": max(json_response_1["message"]["subset"]["general"]["quantities"]["second"],
+                                             json_response_2["message"]["subset"]["general"]["quantities"]["second"]),
+                                "third": max(json_response_1["message"]["subset"]["general"]["quantities"]["third"],
+                                            json_response_2["message"]["subset"]["general"]["quantities"]["third"])
+                            }
+                        }
+                    }
+                }
+            }
+            return processed_data
+        except Exception as e:
+            raise Exception(f"Error processing JSON: {str(e)}")
+
+    def send_processed_json(self, processed_data):
+        try:
+            response = self.session.post(f"{self.base_url}/api/process", json=processed_data)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Error sending processed JSON: {str(e)}")
